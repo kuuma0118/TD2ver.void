3f7a8b2dbcb89d3feda861898e3c05d01f4dae49
k022g0093@g.neec.ac.jp <k022g0093@g.neec.ac.jp>
2024-01-16 17:10:53 +0900

    作成中

---
+++ Project/Game/OverScene/OverScene.cpp
@@ -0,0 +1,145 @@
+#include "OverScene.h"
+#include "GameManager.h"
+#include "TitleScene.h"
+#include "GameScene.h"
+#include <cassert>
+
+GameOverScene::GameOverScene() {};
+
+GameOverScene::~GameOverScene() {};
+
+void GameOverScene::Initialize(GameManager* gameManager)
+{
+	//TextureManagerのインスタンスを取得
+	textureManager_ = TextureManager::GetInstance();
+	//Audioのインスタンスを取得
+	audio_ = Audio::GetInstance();
+	//Inputのインスタンスを取得Q
+	input_ = Input::GetInstance();
+
+	viewProjection_.UpdateMatrix();
+
+	
+	transitionSprite_.reset(Sprite::Create(transitionTextureHandle_, { 0.0f,0.0f }));
+	transitionSprite_->SetColor(transitionColor_);
+	transitionSprite_->SetSize(Vector2{ 640.0f,360.0f });
+};
+
+void GameOverScene::Update(GameManager* gameManager)
+{
+	
+	if (!Input::GetInstance()->GetJoystickState(joyState_))
+	{
+		return;
+	}
+
+	if (joyState_.Gamepad.wButtons & XINPUT_GAMEPAD_A)
+	{
+		if (isTransitionEnd_) {
+			isTransition_ = true;
+			if (soundCount_ == 0)
+			{
+				soundCount_ = 1;
+				audio_->SoundPlayWave(soundHandle_, false);
+			}
+		}
+	}
+
+	if (input_->IsPushKeyEnter(DIK_T))
+	{
+		audio_->StopAudio(overSoundHandle_);
+		gameManager->ChangeScene(new GameTitleScene());
+	}
+
+	if (isTransitionEnd_ == false) {
+		transitionTimer_ += 1.0f / kTransitionTime;
+		transitionColor_.w = Lerp(transitionColor_.w, 0.0f, transitionTimer_);
+		transitionSprite_->SetColor(transitionColor_);
+
+		if (transitionColor_.w <= 0.0f) {
+			isTransitionEnd_ = true;
+			transitionTimer_ = 0.0f;
+		}
+	}
+
+	if (isTransition_) {
+		transitionTimer_ += 1.0f / kTransitionTime;
+		transitionColor_.w = Lerp(transitionColor_.w, 1.0f, transitionTimer_);
+		transitionSprite_->SetColor(transitionColor_);
+
+		if (transitionColor_.w >= 1.0f) {
+			audio_->StopAudio(overSoundHandle_);
+			gameManager->ChangeScene(new GameScene);
+		}
+	}
+
+	playerWorldTransform_.UpdateMatrix();
+	weaponWorldTransform_.UpdateMatrix();
+	bossWorldTransform_.UpdateMatrix();
+
+	viewProjection_.UpdateMatrix();
+};
+
+void GameOverScene::Draw(GameManager* gameManager)
+{
+	PostProcess::GetInstance()->PreDraw();
+
+#pragma region 背景スプライトの描画
+
+	//背景スプライトの描画
+	Sprite::PreDraw(Sprite::kBlendModeNormal);
+
+	Sprite::PostDraw();
+
+#pragma endregion
+
+	//深度バッファをクリア
+	DirectXCommon::GetInstance()->ClearDepthBuffer();
+
+#pragma region モデルの描画
+
+	//モデルの描画
+	Model::PreDraw();
+
+	//背景の描画
+	backGround_->Draw(viewProjection_);
+
+	playerModel_->Draw(playerWorldTransform_, viewProjection_);
+
+	weaponModel_->Draw(weaponWorldTransform_, viewProjection_);
+
+	bossModel_->Draw(bossWorldTransform_, viewProjection_);
+
+	Model::PostDraw();
+
+#pragma endregion
+
+#pragma region パーティクルの描画
+
+	//パーティクルモデルの描画
+	ParticleModel::PreDraw();
+
+	ParticleModel::PostDraw();
+
+#pragma endregion
+
+	PostProcess::GetInstance()->PostDraw();
+
+#pragma region 前景スプライトの描画
+
+	//スプライトの描画処理
+	Sprite::PreDraw(Sprite::kBlendModeNormal);
+
+	loseSprite_->Draw();
+
+	x4x3Sprite_->Draw();
+
+	pressASprite_->Draw();
+
+	transitionSprite_->Draw();
+
+	Sprite::PostDraw();
+
+#pragma endregion
+
+};
